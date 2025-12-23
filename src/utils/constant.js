d17dab2eee16d4b873d5eafed5d3404c6b1e7499
praneesshh <praneshkumar.n2023it@sece.ac.in>
2025-12-23 14:39:01 +0530

    1

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -12,7 +12,7 @@ console.log('🔍 Environment Debug:');
 console.log('VITE_API_BASE_URL:', import.meta.env.VITE_API_BASE_URL);
 console.log('API_BASE_URL:', API_BASE_URL);
 
-export const USER_API_END_POINT = `${API_BASE_URL}/api/v1/user`;
-export const JOB_API_END_POINT = `${API_BASE_URL}/api/v1/job`;
-export const APPLICATION_API_END_POINT = `${API_BASE_URL}/api/v1/application`;
-export const COMPANY_API_END_POINT = `${API_BASE_URL}/api/v1/company`;
+export const USER_API_END_POINT = `user`;
+export const JOB_API_END_POINT = `job`;
+export const APPLICATION_API_END_POINT = `application`;
+export const COMPANY_API_END_POINT = `company`;
